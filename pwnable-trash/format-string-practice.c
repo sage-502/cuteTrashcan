d1782d7287570a2741d7c93340b905c06af8fbe3
sage <jjhcat0402@gmail.com>
2026-01-09 17:40:24 +0900

    Add format-string-practice.c file
    Implement format string vulnerability example.

---
+++ pwnable-trash/format-string-practice.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int target = 0xcafebabe;
+    char buf[100];
+
+    printf("target addr = %p\n", &target);
+    fgets(buf, sizeof(buf), stdin);
+    printf(buf);
+    printf("\ntarget = 0x%x\n", target);
+
+    if(target == 0xdeadbeef){
+	printf("good!\n");
+        system("/bin/bash");
+    }	    
+
+    return 0;
+}
